adb413fea465cfe33df28d1f99efdcc2975e3122
Matthew Muller <1037502@students.wits.ac.za>
2018-08-03 15:32:45 +0200

    Exercise 2.1

--- src/vector/size_capacity.cpp
+++ src/vector/size_capacity.cpp
@@ -1,3 +1,13 @@
+//Matthew Muller 1037502 Software Lab 2 Exercise 2.1
+
+/* The output of lines 4 and 5 should both be zero, because there hasn't been any memory
+ * allocated for the ints to be stored in the vector yet and no ints have been added yet. 
+ * Once the vector begins being filled with integers it will increase in size (the number 
+ * of elements stored) and it will dynamically change its capacity to accomodate more 
+ * elements. The capacity (or pre-allocated space in memory) doubles each time the size 
+ * catches up to the capacity.
+ * */
+
 // size_capacity.cpp
 // Vector size versus capacity
 
